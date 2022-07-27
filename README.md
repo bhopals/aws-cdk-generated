e403776d740b06d8b338a4f9d59ba15ef0a6fcc1
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-07-27 12:16:04 -0400

    cdk generated code

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# aws-cdk-generated
+# Welcome to your CDK TypeScript project
+
+This is a blank project for CDK development with TypeScript.
+
+Project is generated using
+
+```
+cdk init app --language typescript
+```
+
+Refer for more details - https://docs.aws.amazon.com/cdk/v2/guide/work-with-cdk-typescript.html
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+## Useful commands
+
+- `cdk bootstrap` bootstrapin (init) the stack.
+
+- `cdk synth` emits the synthesized CloudFormation template
+
+- `cdk deploy` deploy this stack to your default AWS account/region
+
+- `cdk destroy` remove the stack
---
+++ test/aws-cdk-generated.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkGenerated from '../lib/aws-cdk-generated-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-generated-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkGenerated.AwsCdkGeneratedStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
